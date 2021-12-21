1408fe73a6b35ca4d4310dd2dcce5c153743b994
jonathan bola <bolajonathan@yahoo.com>
2021-12-21 16:32:31 -0600

    taskLister

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+
+  const form = document.querySelector('form')
+  form.addEventListener('submit', (event) =>{
+    event.preventDefault()
+    let p = document.createElement('p')
+    let input = document.querySelector('#new-task-description').value
+    let div = document.querySelector('#main-content')
+    p.textContent = input;
+    let btn = document.createElement('button')
+    btn.textContent = 'X'
+    btn.addEventListener('click', ()=>{
+      p.remove()
+    })
+    p.appendChild(btn)
+
+
+    div.appendChild(p)
+
+    form.reset()
+
+  })
+  
+ 
+})
+ 
